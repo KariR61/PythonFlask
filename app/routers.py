97e43e1cf1fd73f4ec996819fe329601cc815971
KariR61 <kariraappana@gmail.com>
2016-01-28 14:51:07 +0200

    new files

--- app/routers.py
+++ app/routers.py
@@ -1,14 +1,12 @@
 from app import app
 #render_template gives you access to Jinja2 template engine
 from flask import render_template,request, make_response
+from app.forms import LoginForm
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    name = 'Markus'
-    address = 'Rautatienkatu'
-    response = make_response(render_template('template_index.html',name=name,title=address))
-    response.headers.add('Cache_Control','no-cache')
-    return response
+    login = LoginForm()
+    return render_template('template_index.html',form=login)
 
 @app.route('/user/<name>')
 def user(name):
